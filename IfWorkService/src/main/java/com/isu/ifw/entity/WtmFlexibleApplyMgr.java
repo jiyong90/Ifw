41111fd4fd14d2fb77a757fc663ceaf2a92a3e36
user <user@DESKTOP-0AO5105>
2019-10-10 10:25:38 +0900

    .

--- IfWorkService/src/main/java/com/isu/ifw/entity/WtmFlexibleApplyMgr.java
+++ IfWorkService/src/main/java/com/isu/ifw/entity/WtmFlexibleApplyMgr.java
@@ -50,7 +50,7 @@ public class WtmFlexibleApplyMgr {
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date updateDate;
 	@Column(name="UPDATE_ID")
-	private Long updateId;
+	private String updateId;
 	
 	
 	public Long getFlexibleApplyId() {
@@ -131,10 +131,10 @@ public class WtmFlexibleApplyMgr {
 	public void setUpdateDate(Date updateDate) {
 		this.updateDate = updateDate;
 	}
-	public Long getUpdateId() {
+	public String getUpdateId() {
 		return updateId;
 	}
-	public void setUpdateId(Long updateId) {
+	public void setUpdateId(String updateId) {
 		this.updateId = updateId;
 	}
 	
--- IfWorkService/src/main/java/com/isu/ifw/entity/WtmWorkPattDet.java
+++ IfWorkService/src/main/java/com/isu/ifw/entity/WtmWorkPattDet.java
@@ -36,7 +36,7 @@ public class WtmWorkPattDet {
 	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private Date updateDate;
 	@Column(name="UPDATE_ID")
-	private Long updateId;
+	private String updateId;
 
 	
 	public Long getWorkPattDetId() {
@@ -75,10 +75,10 @@ public class WtmWorkPattDet {
 	public void setUpdateDate(Date updateDate) {
 		this.updateDate = updateDate;
 	}
-	public Long getUpdateId() {
+	public String getUpdateId() {
 		return updateId;
 	}
-	public void setUpdateId(Long updateId) {
+	public void setUpdateId(String updateId) {
 		this.updateId = updateId;
 	}
 	
