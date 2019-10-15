65557319a24f086e7afb10f4ef80b56919c190b2
claire <claire@10.30.30.188>
2019-10-15 15:21:03 +0900

    validation api

--- IfWorkService/src/main/java/com/isu/ifw/controller/WtmApiController.java
+++ IfWorkService/src/main/java/com/isu/ifw/controller/WtmApiController.java
@@ -37,13 +37,13 @@ public class WtmApiController extends TenantSecuredControl {
 	}
 	
 	@RequestMapping(value="/worktime/valid", method = RequestMethod.GET)
-	public ReturnParam workValid(@RequestParam(required=true) String apiKey,
+	public ReturnParam worktimeValid(@RequestParam(required=true) String apiKey,
 								@RequestParam(required=true) String secret,
 								@RequestParam(required=true) String enterCd,
 								@RequestParam(required=true) String sabun,
 								@RequestParam(required=true) String sdate,
 								@RequestParam(required=true) String edate,
-								@RequestParam(required=false) Long applId,
+								@RequestParam(required=false) String applId,
 			HttpServletRequest request ){
 		ReturnParam rp = new ReturnParam();
 		rp.setSuccess("");
@@ -84,10 +84,59 @@ public class WtmApiController extends TenantSecuredControl {
 			return rp;
 		}
 		
-		rp = flexibleEmpService.checkDuplicateWorktime(tenantId, enterCd, sabun, sd, ed, applId);
+		
+		rp = flexibleEmpService.checkDuplicateWorktime(tenantId, enterCd, sabun, sd, ed, Long.parseLong(applId));
 		
 		return rp;
 	}
 	
+	
+	@RequestMapping(value="/workappl/valid", method = RequestMethod.GET)
+	public ReturnParam workapplValid(@RequestParam(required=true) String apiKey,
+								@RequestParam(required=true) String secret,
+								@RequestParam(required=true) String enterCd,
+								@RequestParam(required=true) String sabun,
+								@RequestParam(required=true) String symd,
+								@RequestParam(required=true) String eymd,
+								@RequestParam(required=false) String applId,
+			HttpServletRequest request ){
+		ReturnParam rp = new ReturnParam();
+		rp.setSuccess("");
+		try {
+			this.certificate(apiKey, secret, request.getRemoteHost());
+		} catch (CertificateException e1) {
+			rp.setFail(e1.getMessage());
+			return rp;
+		}
+		
+		String f = "yyyyMMdd";
+		if(symd.length() != eymd.length()) {
+			rp.setFail("시작시분과 종료시분에 정보의 길이가 맞지 않습니다.");
+			return rp;
+		}
+		
+		if(symd.length() != 8) {
+			rp.setFail("일자 정보의 포맷은 yyyyMMddHHmm 또는 yyyyMMddHHmmss 입니다.");
+			return rp;
+		}
+		Long tenantId = this.getTenantId(apiKey);
+		SimpleDateFormat sdf = new SimpleDateFormat(f);
+		Date sd, ed;
+		try {
+			sd = sdf.parse(symd);
+			ed = sdf.parse(eymd);
+		} catch (ParseException e) { 
+			rp.setFail("일자 정보의 포맷은 yyyyMMdd 입니다.");
+			return rp;
+		}
+		
+		if(sd.compareTo(ed) > 0) {
+			rp.setFail("시작일자가 종료일자보다 큽니다.");
+			return rp;
+		}
+		rp = flexibleEmpService.checkDuplicateFlexibleWork(tenantId, enterCd, sabun, symd, eymd, Long.parseLong(applId));
+		
+		return rp;
+	}
 	
 }
