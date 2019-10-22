6d1e5785fdca3ea6bcde55707df7ffb63369e308
ksj0222 <ksj0222@gmail.com>
2019-10-22 16:03:57 +0900

    모바일 출퇴근

--- IfWorkService/src/main/java/com/isu/ifw/controller/WtmInoutController.java
+++ IfWorkService/src/main/java/com/isu/ifw/controller/WtmInoutController.java
@@ -57,15 +57,22 @@ public class WtmInoutController {
 		Map <String,Object> resultMap = new HashMap<String,Object>();
 		Map <String,Object> returnMap = new HashMap<String,Object>();
 		//Map<String,Object> menus = inoutService.getMenuContext(tenantId, enterCd, sabun); 
-		Map<String,Object> menuIn = new HashMap();
-		menuIn.put("inoutType", "IN");
+//		Map<String,Object> menuIn = new HashMap();
+//		menuIn.put("inoutType", "IN");
 	
-		Map<String,Object> menuOut = new HashMap();
-		menuOut.put("inoutType", "OUT");
+//		Map<String,Object> menuOut = new HashMap();
+//		menuOut.put("inoutType", "OUT");
 		
-		returnMap.put("D01", menuIn);
-		returnMap.put("D02", menuOut);
-		resultMap.put("menus", returnMap);
+		
+		//Map<String,Object> menus = inoutService.getMenuContext(tenantId, enterCd, sabun); 
+
+		Map<String,Object> menus = inoutService.getMenuContext(tenantId, enterCd, sabun); 
+		
+//		returnMap.put("D01", menuIn);
+//		returnMap.put("D02", menuOut);
+//		returnMap.put("D02", menus);
+		
+		resultMap.put("menus", menus);
 		rp.put("result", resultMap);
 		
 		System.out.println("status : " + rp.toString());
