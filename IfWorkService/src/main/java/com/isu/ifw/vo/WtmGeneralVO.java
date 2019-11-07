fd8505cb74ce58c1823665d49672f4d1df258213
SEOJOO <SEOJOO@DESKTOP-390KCIM>
2019-11-07 18:20:52 +0900

    .

--- IfWorkService/src/main/java/com/isu/ifw/vo/WtmGeneralVO.java
+++ IfWorkService/src/main/java/com/isu/ifw/vo/WtmGeneralVO.java
@@ -9,21 +9,23 @@ public class WtmGeneralVO extends HashMap {
 		
 		String k = (String)key;
 		
+		System.out.println("k ::::::::::: " + k);
+		
 		try {
 			// _ to camelCase
 			if(k !=null && !"".equals(k)) {
-				//if(!k.contains("_")) {
-				//	k = k.toLowerCase();
-				//} else {
-					while(k.contains("_")) {
-			            k = k.replaceFirst("_[a-z]", String.valueOf(Character.toUpperCase(k.charAt(k.indexOf("_") + 1))));
-			        }
-				//}
+				k = k.toLowerCase();
+				
+				while( k.lastIndexOf("_")!=k.length()-1 && k.contains("_")) {
+		            k = k.replaceFirst("_[a-z]", String.valueOf(Character.toUpperCase(k.charAt(k.indexOf("_") + 1))));
+		        }
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
 			k = (String)key;
 		}
+		
+		System.out.println("change k ::::::::::: " + k);
 		
 		return super.put(k, value);
 	}
