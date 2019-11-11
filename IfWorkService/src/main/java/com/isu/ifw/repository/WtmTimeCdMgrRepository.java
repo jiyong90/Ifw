996bb7b1792e28d215f8ef40dede6c0fcabd4f46
user <user@DESKTOP-0AO5105>
2019-11-11 18:49:27 +0900

    oracle 수정사항 갱신

--- IfWorkService/src/main/java/com/isu/ifw/repository/WtmTimeCdMgrRepository.java
+++ IfWorkService/src/main/java/com/isu/ifw/repository/WtmTimeCdMgrRepository.java
@@ -15,7 +15,7 @@ public interface WtmTimeCdMgrRepository extends JpaRepository<WtmTimeCdMgr, Long
 	//public List<WtmTimeCdMgr> findByTenantIdAndEnterCd(@Param(value="tenantId")Long tenantId, @Param(value="enterCd")String enterCd, @Param(value="sYmd")String sYmd);
 	
 	//@Query(value="SELECT * FROM WTM_TIME_CD_MGR WHERE TENANT_ID = :tenantId AND ENTER_CD = :enterCd AND HOL_YN = :holYn AND date_format(now(),'%Y%m%d') BETWEEN SYMD AND F_WTM_NVL(EYMD, '99991231') ORDER BY SYMD, TIME_CD", nativeQuery = true)
-	@Query(value="SELECT M FROM WtmTimeCdMgr M WHERE M.tenantId = :tenantId AND M.enterCd = :enterCd AND M.holYn = :holYn AND :ymd BETWEEN M.symd AND CASE WHEN M.eymd IS NULL THEN '99991231' ELSE M.eymd END ORDER BY M.symd, M.timeCd")
+	@Query(value="SELECT M FROM WtmTimeCdMgr M WHERE M.tenantId = :tenantId AND M.enterCd = :enterCd AND M.holYn like F_WTM_NVL(:holYn, '%') AND :ymd BETWEEN M.symd AND CASE WHEN M.eymd IS NULL THEN '99991231' ELSE M.eymd END ORDER BY M.symd, M.timeCd")
 	public List<WtmTimeCdMgr> findByTenantIdAndEnterCdAndHolYnAndYmd(@Param(value="tenantId")Long tenantId, @Param(value="enterCd")String enterCd, @Param(value="holYn")String holYn, @Param(value="ymd")String ymd);
 	
 }
