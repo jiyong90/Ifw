99a23f3b4befe03a678269a563c418e413c58c45
user <user@DESKTOP-0AO5105>
2019-11-11 16:41:30 +0900

    오라클 오류수정

--- IfWorkService/src/main/java/com/isu/ifw/repository/WtmBaseWorkMgrRepository.java
+++ IfWorkService/src/main/java/com/isu/ifw/repository/WtmBaseWorkMgrRepository.java
@@ -3,6 +3,7 @@ package com.isu.ifw.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.isu.ifw.entity.WtmBaseWorkMgr;
@@ -11,5 +12,6 @@ import com.isu.ifw.entity.WtmBaseWorkMgr;
 public interface WtmBaseWorkMgrRepository extends JpaRepository<WtmBaseWorkMgr, Long> {
 	
 	//@Query(value="SELECT * FROM WTM_BASE_WORK_MGR WHERE TENANT_ID = :tenantId AND ENTER_CD = :enterCd AND IF(:sYmd='',date_format(now(),'%Y%m%d'),REPLACE(:sYmd, '-', '')) BETWEEN SYMD AND EYMD ", nativeQuery = true)
+	@Query("SELECT M FROM WtmBaseWorkMgr M WHERE M.tenantId = ?1 AND M.enterCd = ?2 AND (?3 BETWEEN M.symd AND M.eymd)")
 	public List<WtmBaseWorkMgr> findByTenantIdAndEnterCdAndSymd(Long tenantId, String enterCd, String sYmd);
 }
