04e197c1b03bda9298a10ff4ab4b4534d4bdc7a8
claire <claire@10.30.30.188>
2020-01-31 19:05:37 +0900

    .

--- IfWorkService/src/main/java/com/isu/ifw/oauth/ResourceServerConfig.java
+++ IfWorkService/src/main/java/com/isu/ifw/oauth/ResourceServerConfig.java
@@ -62,7 +62,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		.and()
 		.cors().disable()
 		.authorizeRequests()
-		.antMatchers("/intf/**", "/login/**","/login/**/authorize","/we/**","/info/**","/certificate/**").permitAll()
+		.antMatchers("/intf/**", "/login/**","/login/**/authorize","/we/**","/info/**","/certificate/**","/schedule").permitAll()
 		.anyRequest()//.access("hasRole('ROLE_USER')")
 		.authenticated()
 		.and()
