7bec02f4cd1d5fe4ddb15e10fa5ca3335149a51a
ksj0222 <ksj0222@gmail.com>
2020-02-24 16:59:00 +0900

    확정 트랜잭션 처리 추가

--- IfWorkService/src/main/java/com/isu/ifw/controller/WtmFlexibleApplyMgrController.java
+++ IfWorkService/src/main/java/com/isu/ifw/controller/WtmFlexibleApplyMgrController.java
@@ -3,14 +3,13 @@ package com.isu.ifw.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.jboss.logging.MDC;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.isu.ifw.mapper.WtmFlexibleApplyMgrMapper;
+import com.isu.ifw.service.WtmApplService;
 import com.isu.ifw.service.WtmFlexibleApplyMgrService;
 import com.isu.ifw.util.WtmUtil;
 import com.isu.ifw.vo.ReturnParam;
@@ -31,7 +32,15 @@ public class WtmFlexibleApplyMgrController {
 	@Autowired
 	WtmFlexibleApplyMgrService flexibleApplyService;
 	
-	private final Logger logger = LoggerFactory.getLogger("ifwDBLog");
+	
+	@Autowired
+	WtmFlexibleApplyMgrMapper wtmFlexibleApplyMgrMapper;
+	
+	@Autowired
+	@Qualifier("wtmFlexibleApplService")
+	WtmApplService wtmApplService;
+	
+	private final Logger logger = LoggerFactory.getLogger("ifwFileLog");
 	
 	@RequestMapping(value="/list", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody ReturnParam getApplyList(HttpServletRequest request, @RequestParam Map<String, Object> paramMap ) throws Exception {
@@ -41,6 +50,8 @@ public class WtmFlexibleApplyMgrController {
 		Map<String, Object> sessionData = (Map<String, Object>) request.getAttribute("sessionData");
 		String enterCd = sessionData.get("enterCd").toString();
 		String sYmd = paramMap.get("sYmd").toString();
+		
+		logger.debug("[getApplyList] " + tenantId + enterCd + sYmd);
 		
 		rp.setSuccess("");
 		
@@ -53,7 +64,8 @@ public class WtmFlexibleApplyMgrController {
 			rp.setFail("조회 시 오류가 발생했습니다.");
 			return rp;
 		}
-		
+		logger.debug("[getApplyList rp] " + rp.toString());
+
 		return rp;
 	}
 	
@@ -70,20 +82,13 @@ public class WtmFlexibleApplyMgrController {
 		String userId = sessionData.get("userId").toString();
 		
 		
-		MDC.put("sessionId", request.getSession().getId());
-		MDC.put("logId", UUID.randomUUID().toString());
-		MDC.put("type", "C");
-		MDC.put("param", paramMap.toString());
-		logger.debug("setApplyList Controller Start", MDC.get("sessionId"), MDC.get("logId"), MDC.get("type"));
-		
 		Map<String, Object> convertMap = WtmUtil.requestInParamsMultiDML(request,paramMap.get("s_SAVENAME").toString(),"");
 		convertMap.put("enterCd", enterCd);
 		convertMap.put("tenantId", tenantId);
 		convertMap.put("userId", userId);
 
-		MDC.put("convertMap", convertMap);
+		logger.debug("[setApplyList] " + convertMap.toString());
 
-		
 		rp.setSuccess("");
 		int cnt = 0;
 		try {		
@@ -95,7 +100,8 @@ public class WtmFlexibleApplyMgrController {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+		logger.debug("[setApplyList rp] ", rp.toString());
+
 		return rp;
 	}
 	
@@ -106,7 +112,8 @@ public class WtmFlexibleApplyMgrController {
 		Long tenantId = Long.valueOf(request.getAttribute("tenantId").toString());
 		Map<String, Object> sessionData = (Map<String, Object>) request.getAttribute("sessionData");
 		String enterCd = sessionData.get("enterCd").toString();
-		
+		logger.debug("[getEymd] " + tenantId +enterCd);
+
 		rp.setSuccess("");
 		
 		Map<String, Object> codeMap = null;
@@ -118,7 +125,8 @@ public class WtmFlexibleApplyMgrController {
 			rp.setFail("조회 시 오류가 발생했습니다.");
 			return rp;
 		}
-		
+		logger.debug("[getEymd] " + rp.toString());
+
 		return rp;
 	}
 	
@@ -130,7 +138,8 @@ public class WtmFlexibleApplyMgrController {
 		Map<String, Object> sessionData = (Map<String, Object>) request.getAttribute("sessionData");
 		String enterCd = sessionData.get("enterCd").toString();
 		Long flexibleStdMgrId = Long.valueOf(paramMap.get("flexibleStdMgrId").toString());
-		
+		logger.debug("[getworkType] " + tenantId +enterCd+flexibleStdMgrId);
+
 		rp.setSuccess("");
 		
 		List<Map<String, Object>> codeList = null;
@@ -142,18 +151,16 @@ public class WtmFlexibleApplyMgrController {
 			rp.setFail("조회 시 오류가 발생했습니다.");
 			return rp;
 		}
-		
+		logger.debug("[getworkType rp] " + rp.toString());
+
 		return rp;
 	}
-	
-	
-	
 	
 	@RequestMapping(value="/apply", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody ReturnParam setApply(HttpServletRequest request, @RequestParam Map<String, Object> paramMap ) throws Exception {
 		
 		ReturnParam rp = new ReturnParam();
-		rp.setFail("확정 시 오류가 발생했습니다.");
+		rp.setSuccess("확정에 성공하였습니다.");
 		try {
 			Long tenantId = Long.valueOf(request.getAttribute("tenantId").toString());
 			Map<String, Object> sessionData = (Map<String, Object>) request.getAttribute("sessionData");
@@ -164,27 +171,58 @@ public class WtmFlexibleApplyMgrController {
 			paramMap.put("enterCd", enterCd);
 			paramMap.put("userId", userId);
 			
-			MDC.put("sessionId", request.getSession().getId());
-			MDC.put("logId", UUID.randomUUID().toString());
-			MDC.put("type", "C");
-			MDC.put("param", paramMap.toString());
-			logger.debug("setApply Controller Start", MDC.get("sessionId"), MDC.get("logId"), MDC.get("type"));
+			logger.debug("[setApply] ", paramMap.toString());
 			
-			Map<String, Object> convertMap = new HashMap();
-			convertMap.put("enterCd", enterCd);
-			convertMap.put("tenantId", tenantId);
-			convertMap.put("userId", userId);
+			String workTypeCd = paramMap.get("workTypeCd").toString();
+			Long flexibleApplyId = Long.valueOf(paramMap.get("flexibleApplyId").toString());
 			
-			rp = flexibleApplyService.setApply(paramMap);
-	
-			MDC.put("convertMap", convertMap);
+			// 확정대상자 조회
+			List<Map<String, Object>> searchList =  wtmFlexibleApplyMgrMapper.getApplyConfirmList(paramMap);
+			if(searchList==null || searchList.size()==0) {
+				rp.setFail("확정 대상자가 없습니다.");
+				return rp;
+			}
+			
+			logger.debug("[setApply] 확정대상자 " + searchList.size());
+			
+			//반복기준조회
+			List<Map<String, Object>> ymdList = flexibleApplyService.getApplyYmdList(paramMap);
+			
+			//오류체크까지 하고 리턴
+			for(int i=0; i < searchList.size(); i++) {
+				Map<String, Object> validateMap = new HashMap<>();
+				validateMap = searchList.get(i);
+				String sabun = validateMap.get("sabun").toString();
+				
+				for(int j=0; j < ymdList.size(); j++) {
+					// 반복 구간별 밸리데이션 체크 및 유연근무기간 입력
+					paramMap.put("sYmd", ymdList.get(j).get("symd"));
+					paramMap.put("eYmd", ymdList.get(j).get("eymd"));
+					
+					//탄근제 validation 체크를 위한 param
+					paramMap.put("adminYn", "Y");
+					paramMap.put("flexibleApplyId", flexibleApplyId);
+					
+					rp = wtmApplService.validate(tenantId, enterCd, sabun, workTypeCd, paramMap);
+					if(rp.getStatus().equals("FAIL")) {
+						return rp;
+					}
+					
+					validateMap.put("flexibleApplyId", flexibleApplyId);
+					validateMap.put("workTypeCd", workTypeCd);
+				}
+			}
+			
+			//비동기로 확정처리
+			//flexibleApplyService.setApplyAsync(searchList, ymdList);
+			//동기로 확정처리
+			int cnt = flexibleApplyService.setApply(searchList, ymdList);
+			rp.setMessage("총 " + searchList.size() + "명의 확정 대상자 중 " + cnt + "명의 확정에 성공하였습니다.");
 		} catch(Exception e) {
-			rp.setStatus("FAIL");
 			rp.setFail("확정 시 오류가 발생했습니다.");
-			return rp;
-		}
-		
-		//rp.setSuccess("");
+		} 
+
+		logger.debug("[setApply rp] " + rp.toString());
 		return rp;
 	}
 	
@@ -223,21 +261,13 @@ public class WtmFlexibleApplyMgrController {
 		String empNo = sessionData.get("empNo").toString();
 		String userId = sessionData.get("userId").toString();
 		Long flexibleApplyId = Long.valueOf(paramMap.get("flexibleApplyId").toString());
-		
-		MDC.put("sessionId", request.getSession().getId());
-		MDC.put("logId", UUID.randomUUID().toString());
-		MDC.put("type", "C");
-		MDC.put("param", paramMap.toString());
-		logger.debug("setApplyList Controller Start", MDC.get("sessionId"), MDC.get("logId"), MDC.get("type"));
-		
+		logger.debug("[setApplyGrpList] " + paramMap.toString());
+
 		Map<String, Object> convertMap = WtmUtil.requestInParamsMultiDML(request,paramMap.get("s_SAVENAME").toString(),"");
 		convertMap.put("enterCd", enterCd);
 		convertMap.put("tenantId", tenantId);
 		convertMap.put("userId", userId);
 
-		MDC.put("convertMap", convertMap);
-
-		
 		rp.setSuccess("");
 		int cnt = 0;
 		try {		
@@ -249,7 +279,8 @@ public class WtmFlexibleApplyMgrController {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+		logger.debug("[setApplyGrpList rp] " + rp.toString());
+
 		return rp;
 	}
 	
@@ -288,21 +319,13 @@ public class WtmFlexibleApplyMgrController {
 		String empNo = sessionData.get("empNo").toString();
 		String userId = sessionData.get("userId").toString();
 		Long flexibleApplyId = Long.valueOf(paramMap.get("flexibleApplyId").toString());
-		
-		MDC.put("sessionId", request.getSession().getId());
-		MDC.put("logId", UUID.randomUUID().toString());
-		MDC.put("type", "C");
-		MDC.put("param", paramMap.toString());
-		logger.debug("setApplyList Controller Start", MDC.get("sessionId"), MDC.get("logId"), MDC.get("type"));
-		
+		logger.debug("[setApplyEmpList] " + paramMap.toString());
+
 		Map<String, Object> convertMap = WtmUtil.requestInParamsMultiDML(request,paramMap.get("s_SAVENAME").toString(),"");
 		convertMap.put("enterCd", enterCd);
 		convertMap.put("tenantId", tenantId);
 		convertMap.put("userId", userId);
 
-		MDC.put("convertMap", convertMap);
-
-		
 		rp.setSuccess("");
 		int cnt = 0;
 		try {		
@@ -314,7 +337,8 @@ public class WtmFlexibleApplyMgrController {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+		logger.debug("[setApplyEmpList rp] " + rp.toString());
+
 		return rp;
 	}
 	
