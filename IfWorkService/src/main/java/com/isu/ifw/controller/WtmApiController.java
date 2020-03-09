a562fb64ab71e6673f6424690dead14595c0865f
SEOJOO <SEOJOO@DESKTOP-390KCIM>
2020-03-09 17:41:14 +0900

    근태 신청 validation 수정.

--- IfWorkService/src/main/java/com/isu/ifw/controller/WtmApiController.java
+++ IfWorkService/src/main/java/com/isu/ifw/controller/WtmApiController.java
@@ -1,11 +1,15 @@
 package com.isu.ifw.controller;
 
+import java.security.InvalidParameterException;
 import java.security.cert.CertificateException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -379,45 +383,121 @@ public class WtmApiController extends TenantSecuredControl {
 	
 	@RequestMapping(value="/worktime/appl/valid", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ReturnParam worktimeApplValid(@RequestBody Map<String, Object> paramMap, HttpServletRequest request ){
+		
 		ReturnParam rp = new ReturnParam();
 		rp.setSuccess("");
+		
 		try {
-			if(!paramMap.containsKey("apiKey") || paramMap.get("apiKey")==null || "".equals(paramMap.get("apiKey")) ) {
-				rp.setFail("apiKey가 없습니다.");
-				return rp;
-			}
-			if(!paramMap.containsKey("secret") || paramMap.get("secret")==null || "".equals(paramMap.get("secret")) ) {
-				rp.setFail("secret가 없습니다.");
+			validateParamMap(paramMap, "apiKey", "secret", "enterCd", "applNo", "works");
+			
+			try {
+				if(!paramMap.containsKey("apiKey") || paramMap.get("apiKey")==null || "".equals(paramMap.get("apiKey")) ) {
+					rp.setFail("apiKey가 없습니다.");
+					return rp;
+				}
+				if(!paramMap.containsKey("secret") || paramMap.get("secret")==null || "".equals(paramMap.get("secret")) ) {
+					rp.setFail("secret가 없습니다.");
+					return rp;
+				}
+				
+				this.certificate(paramMap.get("apiKey").toString(), paramMap.get("secret").toString(), request.getRemoteHost());
+			} catch (CertificateException e1) {
+				rp.setFail(e1.getMessage());
 				return rp;
 			}
 			
-			this.certificate(paramMap.get("apiKey").toString(), paramMap.get("secret").toString(), request.getRemoteHost());
-		} catch (CertificateException e1) {
-			rp.setFail(e1.getMessage());
-			return rp;
-		}
-		
-		Long tenantId = this.getTenantId(paramMap.get("apiKey").toString());
-		if(!paramMap.containsKey("enterCd") || paramMap.get("enterCd")==null || "".equals(paramMap.get("enterCd")) ) {
-			rp.setFail("회사 코드가 없습니다.");
-			return rp;
-		}
-		
-		Long applId = null;
-		if(paramMap.containsKey("applId") && paramMap.get("applId")!=null && !"".equals(paramMap.get("applId")) ) {
-			applId = Long.parseLong(paramMap.get("applId").toString());
-		}
-		
-		if(!paramMap.containsKey("appl") || paramMap.get("appl")==null || "".equals(paramMap.get("appl")) ) {
-			rp.setFail("근태 정보가 없습니다.");
-			return rp;
-		}
+			Long tenantId = this.getTenantId(paramMap.get("apiKey").toString());
 			
-		ObjectMapper mapper = new ObjectMapper();
-		List<Map<String, Object>> workList = (List<Map<String, Object>>)paramMap.get("appl");
+			String applSabun = null;
+			if(paramMap.containsKey("applSabun") && paramMap.get("applSabun")!=null && !"".equals(paramMap.get("applSabun")) ) {
+				applSabun = paramMap.get("applSabun").toString();
+			}
+				
+			List<Map<String, Object>> works = (List<Map<String, Object>>)paramMap.get("works");
+			
+			rp = validatorService.worktimeValid(tenantId, paramMap.get("enterCd").toString(), paramMap.get("applNo").toString(), works, applSabun);
 		
-		rp = validatorService.worktimeValid(tenantId, paramMap.get("enterCd").toString(), workList, applId);
+		} catch(Exception e) {
+			e.printStackTrace();
+			rp.setFail(e.getMessage());
+			return rp;
+		}
 		
 		return rp;
 	}
+	
+	@RequestMapping(value="/worktime/appl/request", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ReturnParam requestWorktimeAppl(@RequestBody Map<String, Object> paramMap, HttpServletRequest request ){
+		ReturnParam rp = new ReturnParam();
+		rp.setSuccess("");
+		
+		try {
+			validateParamMap(paramMap, "apiKey", "secret", "enterCd", "applNo", "works", "applSabun", "status");
+			
+			try {
+				if(!paramMap.containsKey("apiKey") || paramMap.get("apiKey")==null || "".equals(paramMap.get("apiKey")) ) {
+					rp.setFail("apiKey가 없습니다.");
+					return rp;
+				}
+				if(!paramMap.containsKey("secret") || paramMap.get("secret")==null || "".equals(paramMap.get("secret")) ) {
+					rp.setFail("secret가 없습니다.");
+					return rp;
+				}
+				
+				this.certificate(paramMap.get("apiKey").toString(), paramMap.get("secret").toString(), request.getRemoteHost());
+			} catch (CertificateException e1) {
+				rp.setFail(e1.getMessage());
+				return rp;
+			}
+			
+			Long tenantId = this.getTenantId(paramMap.get("apiKey").toString());
+			
+			String applSabun = null;
+			if(paramMap.containsKey("applSabun") && paramMap.get("applSabun")!=null && !"".equals(paramMap.get("applSabun")) ) {
+				applSabun = paramMap.get("applSabun").toString();
+			}
+				
+			List<Map<String, Object>> works = (List<Map<String, Object>>)paramMap.get("works");
+			
+			rp = validatorService.worktimeValid(tenantId, paramMap.get("enterCd").toString(), paramMap.get("applNo").toString(), works, applSabun);
+			if(rp!=null && rp.getStatus()!=null && "OK".equals(rp.getStatus())) {
+				wtmInterfaceService.setTaaApplArrIf(paramMap);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			rp.setFail(e.getMessage());
+			return rp;
+		}
+		
+		return rp;
+	}
+	
+	/**
+	 * 파라미터 맵의 유효성을 검사한다.
+	 * 파라미터가 다음의 두 조건을 만족하지 않으면, InvalidParameterException를 발생한다.
+	 * 1. map 자체가 null 인 경우
+	 * 2. paramName 배열에 기술된 값이 map에 (하나라도) 키로 존재하지 않는 경우
+	 * 
+	 * @param paramMap
+	 * @param parameterNames
+	 * @throws InvalidParameterException
+	 */
+	protected void validateParamMap(Map<String,Object> paramMap, String...parameterNames )throws InvalidParameterException{
+		
+		// 파라미터가 아무것도 없는 경우에, 아무것도 할 수 없다. 무조건 예외 발생
+		if(paramMap == null)
+			throw new InvalidParameterException("param map is null.");
+		
+		if(parameterNames == null)
+			return; // 파라미터가 없으면 그냥 리턴..
+		
+		// 넘겨 받은 이름 배열이 map의 부분 집합인지를 따짐.
+		Set<String> paramKeySet = paramMap.keySet();
+		Collection<String> params = Arrays.asList(parameterNames);
+		
+		if(!paramKeySet.containsAll(params))
+			throw new InvalidParameterException("required parameter is not found.");
+		
+	}
+	
 }
