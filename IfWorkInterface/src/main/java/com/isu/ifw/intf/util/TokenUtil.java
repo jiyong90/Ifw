02151429a14c036c6a6fe322c04dfbe694ebc620
claire <claire@10.30.30.188>
2020-03-18 16:40:21 +0900

    tokenUtil replace 추

--- IfWorkInterface/src/main/java/com/isu/ifw/intf/util/TokenUtil.java
+++ IfWorkInterface/src/main/java/com/isu/ifw/intf/util/TokenUtil.java
@@ -2,8 +2,11 @@ package com.isu.ifw.intf.util;
 
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.crypto.spec.SecretKeySpec;
@@ -18,6 +21,9 @@ public class TokenUtil {
 
 	//static String signKey = "h0nda20200401k0readziSuinTerFace";
 	static String SIGNATURE_ALGORITHM = "HmacSHA256";
+	
+	private static final List<Integer> REPLACE_IDX  = Arrays.asList(0,1,4,7,10,14,21,44,77,100);
+	
 	//static Long expiredTime = 60000l;
 	
 	public static String createJWT(String signKey, String id, String issuer, String subject, Map<String, Object> payloads, long ttlMillis) {
@@ -53,6 +59,7 @@ public class TokenUtil {
         
     }
 	
+	
 	public static Claims decodeJWT(String signKey, String jwt) {
 	    //This line will throw an exception if it is not a signed JWS (as expected)
 	    Claims claims = Jwts.parser()
@@ -61,13 +68,79 @@ public class TokenUtil {
 	    return claims;
 	}
 	
+	
+	public static String replaceJWT(String token) {
+
+		String replaceToken = "";
+		System.out.println("token : " + token);
+		for(int idx=0; idx < token.length(); idx++) {
+			System.out.print(token.charAt(idx));
+			char tmp = token.charAt(idx);
+			
+			if(REPLACE_IDX.indexOf(idx) != -1) {
+				replaceToken = replaceToken + tmp;
+			}else {
+				if(Character.isUpperCase(tmp)) {
+					replaceToken = replaceToken + Character.toLowerCase(tmp);
+				}else if(Character.isLowerCase(tmp)){
+					replaceToken = replaceToken + Character.toUpperCase(tmp);
+				}else {
+					replaceToken = replaceToken + tmp;
+				}
+			}
+		}
+		System.out.println("token : " + replaceToken);
+		return replaceToken;
+	}
+	
+	public static String createReplaceJWT(String signKey, String id, String issuer, String subject, Map<String, Object> payloads, long ttlMillis) {
+	  	  
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(signKey);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+        
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder()
+                .setClaims(payloads)
+                .setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+      
+        //if it has been specified, let's add the expiration
+        if (ttlMillis > 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }  
+        return replaceJWT(builder.compact());
+        
+    }
+	
+	public static Claims decodeReplaceJWT(String signKey, String jwt) {
+	    //This line will throw an exception if it is not a signed JWS (as expected)
+		String replaceToken = replaceJWT(jwt);
+	    Claims claims = Jwts.parser()
+	            .setSigningKey(DatatypeConverter.parseBase64Binary(signKey))
+	            .parseClaimsJws(replaceToken).getBody();
+	    return claims;
+	}
+	
 	public static void main(String[] args) throws NoSuchAlgorithmException {
 		Map<String, Object> payloads = new HashMap<String, Object>();
 		payloads.put("CD_COMPANY", "1000");
 		payloads.put("NO_EMP", "20081111");
-		//String jwt = createJWT("id","issuer","subject", payloads,60000l);
-		//System.out.println(jwt);
-		//System.out.println(decodeJWT(jwt).toString());
+		String jwt = createReplaceJWT("asdadsa", "id","issuer","subject", payloads,60000l);
+		System.out.println(jwt);
+		System.out.println("ssss");
+		System.out.println(decodeReplaceJWT("asdadsa", jwt).toString());
 	}
 	
 	 
