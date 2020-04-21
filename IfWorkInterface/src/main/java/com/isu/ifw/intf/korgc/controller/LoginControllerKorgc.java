8fa81fe6012af9eaab85bb2746750cb18956160f
claire <claire@10.30.30.188>
2020-04-21 13:34:35 +0900

    korgc interface

--- IfWorkInterface/src/main/java/com/isu/ifw/intf/korgc/controller/LoginControllerKorgc.java
+++ IfWorkInterface/src/main/java/com/isu/ifw/intf/korgc/controller/LoginControllerKorgc.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,8 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
