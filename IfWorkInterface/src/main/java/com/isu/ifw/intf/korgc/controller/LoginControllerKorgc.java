10f44a72e9cec0e7f82ed078dd5327589268f9ca
claire <claire@10.30.30.188>
2020-06-16 08:39:33 +0900

    korgc interface

--- IfWorkInterface/src/main/java/com/isu/ifw/intf/korgc/controller/LoginControllerKorgc.java
+++ IfWorkInterface/src/main/java/com/isu/ifw/intf/korgc/controller/LoginControllerKorgc.java
@@ -31,6 +31,8 @@ import com.isu.ifw.intf.service.ExchangeService;
 import com.isu.ifw.intf.service.GreenMobileApiService;
 import com.isu.ifw.intf.vo.ReturnParam;
 
+import oracle.net.aso.d;
+
 @RestController
 public class LoginControllerKorgc {
 	
@@ -178,6 +180,7 @@ public class LoginControllerKorgc {
 							pMap.put("EmpID", row.get("EmpID"));		
 							
 							Map<String, Object> sessionData = intfMapper.getWtmEmpByEmpID(pMap);
+							sessionData.put("userId", userId);
 							sessionData.put("accessToken", accessToken);
 							pMap.put("sessionData", sessionData);
 							rp.put("result", pMap);
@@ -220,6 +223,206 @@ public class LoginControllerKorgc {
 		System.out.println("#########################/m/certificate/valid " + data);
 		System.out.println("empKey : " + empKey);
 		return service.greenValidEmp(locale, empKey, accessToken);
+	}
+	
+	@RequestMapping(value = "/korgc/emp/rest", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+	public @ResponseBody ReturnParam greenRestCnt(HttpServletRequest request,
+			@RequestParam(value = "locale", required = false) String locale,
+			@RequestParam(value = "sabun", required = false) String sabun,
+			@RequestParam(value = "enterCd", required = false) String enterCd) throws Exception {
+		
+		ReturnParam rp = new ReturnParam();
+		
+		//ObjectMapper mapper = new ObjectMapper();
+		
+		
+		
+		Map<String, Object> pMap = new HashMap<String, Object>();
+		pMap.put("EmpID", sabun);		
+		
+		Map<String, Object> sessionDataMap = intfMapper.getWtmEmpByEmpID(pMap);
+		 
+		//ReturnParam rp = service.greenValidEmp(locale, empKey, accessToken);
+		String userId = sessionDataMap.get("USER_ID").toString();
+		 
+		
+		Map<String, Object> resMap = this.ksystemRest(userId, sabun);;
+		
+		if(resMap.containsKey("Tables") && resMap.get("Tables") != null) {
+			List<Map<String, Object>> tables = (List<Map<String, Object>>) resMap.get("Tables");
+			if(tables.size() > 0) {
+				Map<String, Object> table = tables.get(0);
+				if(table != null && table.containsKey("Rows") && table.get("Rows") != null) {
+					List<Map<String, Object>> rows = (List<Map<String, Object>>) table.get("Rows");
+					if(rows.size() > 0) {
+						Map<String, Object> row = rows.get(0);
+						if(row.containsKey("UserID") && row.get("UserID") != null && (row.get("UserID")+"").equalsIgnoreCase(userId)
+								&& row.containsKey("IsApproval") && row.get("IsApproval") != null && (row.get("IsApproval")+"").equalsIgnoreCase("1")
+										&& row.containsKey("EmpID") && row.get("EmpID") != null
+								) {
+							//rp.putAll(resMap);
+							//"username":"korgc@KORGC@master"
+							Double useCnt = Double.parseDouble(row.get("UseCnt")+"");
+							Double usedCnt = Double.parseDouble(row.get("UsedCnt")+"");
+							Double restCnt = Double.parseDouble(row.get("RestCnt")+"");
+							 
+							pMap.put("useCnt", useCnt);
+							pMap.put("UsedCnt", usedCnt);
+							pMap.put("RestCnt", restCnt);
+							rp.put("DATA", pMap);
+							System.out.println("#########################/rest " + rp.toString());
+							return rp;
+						}
+					}
+				}
+			}
+		}
+		rp.setFail("");
+		return rp;
+	}
+	
+	
+	
+	
+	private Map<String, Object> ksystemRest(String userId, String empId){
+		
+		String url = "http://localhost/Angkor.Ylw.Common.HttpExecute/RestOutsideService.svc/GetServiceMethodJson?";
+		ObjectMapper mapper = new ObjectMapper();
+		/*
+		 * http://211.34.1.10/Angkor.Ylw.Common.HttpExecute/RestOutsideService.svc/GetServiceMethodJson
+		 * ?securityjson={
+		 * certKey : DA949252-7918-46C1-9C9F-D2B60E524A19
+		 * ,userPwd:
+		 * ,methodId : LoginPwdCheck_kpxerp
+		 * ,certId : PWD_CHECK
+		 * ,dsnOper : kpxerp_oper
+		 * ,userId : Master
+		 * ,languageSeq : 1
+		 * ,isDebug : 0
+		 * ,workingTag :
+		 * ,hostIPAddress :
+		 * ,securityType : 0
+		 * ,companySeq : 1
+		 * ,serviceId : IsStoredProcedure
+		 * ,hostComputername :
+		 * ,dsn : kpxerp_oper
+		 * }
+		 */
+//		Map<String, Object> secMap = new HashMap<String, Object>();
+//		secMap.put("certKey", "DA949252-7918-46C1-9C9F-D2B60E524A19");
+//		secMap.put("userPwd", "");
+//		secMap.put("methodId", "LoginPwdCheck_kpxerp");
+//		secMap.put("certId", "PWD_CHECK");
+//		secMap.put("dsnOper", "kpxerp_oper");
+//		secMap.put("userId", paramMap.get("loginUserId"));
+//		secMap.put("languageSeq", "1");
+//		secMap.put("isDebug", "0");
+//		secMap.put("workingTag", "");
+//		secMap.put("hostIPAddress", "");
+//		secMap.put("securityType", "0");
+//		secMap.put("companySeq", "1");
+//		secMap.put("serviceId", "IsStoredProcedure");
+//		secMap.put("hostComputername", "");
+//		secMap.put("dsn", "kpxerp_oper");
+		String securityStr = "certKey:DA949252-7918-46C1-9C9F-D2B60E524A19,"
+				+ "userPwd:,"
+				+ "methodId:LoginPwdCheck_kpxerp,"
+				+ "certId:PWD_CHECK,"
+				+ "dsnOper:kpxerp_oper,"
+				+ "userId:"+userId+","
+				+ "languageSeq:1,"
+				+ "isDebug:0,"
+				+ "workingTag:,"
+				+ "hostIPAddress:,"
+				+ "securityType:0,"
+				+ "companySeq:1,"
+				+ "serviceId:IsStoredProcedure,"
+				+ "hostComputername:,"
+				+ "dsn:kpxerp_oper" 
+				;
+		//System.out.println("secMap : " + secMap.toString());
+		//System.out.println("secMap : " + mapper.writeValueAsString(secMap));
+		//String secStr = mapper.writeValueAsString(secMap);
+		//secStr = secStr.replaceAll("\"", "");
+		//System.out.println(secStr);
+		url += "securityJson=" + securityStr; //URLEncoder.encode(secStr); //mapper.writeValueAsString(secMap);
+		/*
+		 * {"ROOT": {"DataBlock1": [{"UserID":"Master","UserPwd":"1234"}]}
+		 * &dataJson={
+		 * "ROOT":
+		 * {"DataBlock1" : 
+		 * [{
+		 * "UserPwd":"@ini2093","UserID":"Master"}]}}&encryptionType=0&timeOut=30
+		 */
+//		Map<String, Object> rMap = new HashMap<String, Object>();
+//		Map<String, Object> dbMap = new HashMap<String, Object>();
+//		List<Map<String, Object>> dbs = new ArrayList<Map<String,Object>>();
+//		Map<String, Object> uMap = new HashMap<String, Object>();
+//		uMap.put("UserID" , paramMap.get("loginUserId"));
+//		uMap.put("UserPwd" , paramMap.get("loginPassword"));
+//		dbs.add(uMap);
+//		dbMap.put("DataBlock1", dbs);
+//		rMap.put("ROOT", dbMap);
+//		
+//		String rStr = mapper.writeValueAsString(rMap);
+//		//rStr = rStr.replaceAll("\"", "");
+//		System.out.println(rStr);
+		
+		// 공백 있으면 안된다.. url encode 도 쓰면 안된다..
+		String dStr = "{\"ROOT\":{\"DataBlock1\":[{\"EmpId\":\""+empId+"\"}]}}";
+		url += "&dataJson=" + dStr; //URLEncoder.encode(rStr); 
+		
+		url += "&encryptionType=0";
+		url += "&timeOut=30";
+		
+		System.out.println("url : " + url);
+		
+		//키	값
+		/*
+		 * GET /Angkor.Ylw.Common.HttpExecute/RestOutsideService.svc/GetServiceMethodJson
+		 * ?securityJson=certKey:DA949252-7918-46C1-9C9F-D2B60E524A19,userPwd:,methodId:LoginPwdCheck_kpxerp,certId:PWD_CHECK,dsnOper:kpxerp_oper,userId:Master,languageSeq:1,isDebug:0,workingTag:,hostIPAddress:,securityType:0,companySeq:1,serviceId:IsStoredProcedure,hostComputername:,dsn:kpxerp_oper&dataJson={"ROOT":{"DataBlock1":[{"UserPwd":"@ini2093","UserID":"Master"}]}}&encryptionType=0&timeOut=30 
+		 */
+		/**
+		 * { Tables : [
+		 * 	{ TableName :
+		 * 	  Columns : []
+		 * 	  Rows : [{ IsApproval : 1, UserId : master }]
+		 *  } 
+		 * ]} 
+		 */
+		
+		/*
+http://localhost/Angkor.Ylw.Common.HttpExecute/RestOutsideService.svc/GetServiceMethodJson?securityJson=certKey:DA949252-7918-46C1-9C9F-D2B60E524A19,userPwd:,methodId:LoginPwdCheck_kpxerp,certId:PWD_CHECK,dsnOper:kpxerp_oper,userId:Master,languageSeq:1,isDebug:0,workingTag:,hostIPAddress:,securityType:0,companySeq:1,serviceId:IsStoredProcedure,hostComputername:,dsn:kpxerp_oper&dataJson={"ROOT": {"DataBlock1": [{"UserPwd":"@ini2093","UserID":"Master"}]}}&encryptionType=0&timeOut=30
+http://localhost/Angkor.Ylw.Common.HttpExecute/RestOutsideService.svc/GetServiceMethodJson?securityJson=certKey:DA949252-7918-46C1-9C9F-D2B60E524A19,userPwd:,methodId:LoginPwdCheck_kpxerp,certId:PWD_CHECK,dsnOper:kpxerp_oper,userId:Master,languageSeq:1,isDebug:0,workingTag:,hostIPAddress:,securityType:0,companySeq:1,serviceId:IsStoredProcedure,hostComputername:,dsn:kpxerp_oper&dataJson={"ROOT":{"DataBlock1":[{"UserPwd":"@ini2093","UserID":"Master"}]}}&encryptionType=0&timeOut=30 
+		*/
+		//System.out.println(loginUrl);
+		//System.out.println("http://localhost/Angkor.Ylw.Common.HttpExecute/RestOutsideService.svc/GetServiceMethodJson?securityJson=certKey:DA949252-7918-46C1-9C9F-D2B60E524A19,userPwd:,methodId:LoginPwdCheck_kpxerp,certId:PWD_CHECK,dsnOper:kpxerp_oper,userId:Master,languageSeq:1,isDebug:0,workingTag:,hostIPAddress:,securityType:0,companySeq:1,serviceId:IsStoredProcedure,hostComputername:,dsn:kpxerp_oper&dataJson={\"ROOT\":{\"DataBlock1\":[{\"UserPwd\":\"@ini2093\",\"UserID\":\"Master\"}]}}&encryptionType=0&timeOut=30");
+		try {
+			URL obj = new URL(url);
+	        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+	
+	        conn.setRequestProperty("Content-Type", "application/json");
+	        conn.setDoOutput(true);
+	        conn.setRequestMethod("GET");
+	        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+	        String inputLine;
+	        StringBuffer response = new StringBuffer();
+	
+	        while ((inputLine = in.readLine()) != null) {
+	            response.append(inputLine);
+	        }
+	        in.close();
+	        System.out.println("====================================");
+	        System.out.println(response.toString()); //결과, json결과를 parser하여 처리
+	        System.out.println("====================================");
+	         		
+			String r = mapper.readValue(response.toString(), String.class);
+			return mapper.readValue(r, new HashMap().getClass());
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 	private Map<String, Object>  ksystemLogin(String userId, String enterCd, String userPw){
 		String loginUrl = "http://localhost/Angkor.Ylw.Common.HttpExecute/RestOutsideService.svc/GetServiceMethodJson?";
