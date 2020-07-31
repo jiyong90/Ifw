d238cb4992243be5ec6a53822cd6d0cc8765f1c7
claire <claire@10.30.30.188>
2020-07-31 11:29:05 +0900

    .

--- IfWorkService/src/main/java/com/isu/ifw/controller/TestController.java
+++ IfWorkService/src/main/java/com/isu/ifw/controller/TestController.java
@@ -16,9 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.isu.ifw.entity.WtmFlexibleEmp;
 import com.isu.ifw.entity.WtmFlexibleStdMgr;
+import com.isu.ifw.entity.WtmWorkDayResult;
 import com.isu.ifw.mapper.WtmFlexibleStdMapper;
 import com.isu.ifw.repository.WtmFlexibleEmpRepository;
 import com.isu.ifw.repository.WtmFlexibleStdMgrRepository;
+import com.isu.ifw.repository.WtmWorkDayResultRepository;
+import com.isu.ifw.service.WtmApplService;
+import com.isu.ifw.service.WtmCalcService;
 import com.isu.ifw.service.WtmFlexibleEmpService;
 import com.isu.ifw.service.WtmInterfaceService;
 
@@ -40,6 +44,13 @@ public class TestController {
 	@Autowired
 	WtmFlexibleEmpRepository flexEmpRepo;
 	
+	@Autowired
+	WtmFlexibleStdMgrRepository flexStdMgrRepo;
+	
+	@Autowired WtmCalcService calcService;
+	
+	@Autowired WtmWorkDayResultRepository workDayResultRepo;
+	
 	@RequestMapping(value = "/login/calcAppr", method = RequestMethod.GET)
 	public Map<String, Object> testCalcAppr(@RequestParam String enterCd,
 										@RequestParam Long tenantId,
@@ -51,6 +62,69 @@ public class TestController {
 		Map<String, Object> m = new HashMap<String, Object>();
 		empService.calcApprDayInfo(tenantId, enterCd, ymd, ymd, sabun);
 		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("tenantId", tenantId);
+		paramMap.put("enterCd", enterCd);
+		paramMap.put("sabun", sabun);
+		paramMap.put("pId", "JYP");
+		paramMap.put("symd", ymd);
+		paramMap.put("eymd", ymd);
+		empService.createWorkTermtimeByEmployee(tenantId, enterCd, sabun, paramMap, "JSP");
+		return m;
+	}
+	
+	@RequestMapping(value = "/login/inout", method = RequestMethod.GET)
+	public Map<String, Object> testCalcAppr2(@RequestParam String enterCd,
+										@RequestParam Long tenantId,
+										@RequestParam String sabun,
+										@RequestParam String ymd,
+										HttpServletRequest request, 
+										HttpServletResponse response){
+		
+		Map<String, Object> m = new HashMap<String, Object>();
+		empService.calcApprDayInfo(tenantId, enterCd, ymd, ymd, sabun);
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("tenantId", tenantId);
+		paramMap.put("enterCd", enterCd);
+		paramMap.put("sabun", sabun);
+		paramMap.put("pId", "JYP");
+		paramMap.put("sYmd", ymd);
+		paramMap.put("eYmd", ymd);
+		paramMap.put("symd", ymd);
+		paramMap.put("eymd", ymd);
+		
+		   // create result 호출
+        WtmFlexibleEmp flexEmp = flexEmpRepo.findByTenantIdAndEnterCdAndSabunAndYmdBetween(tenantId, enterCd, sabun, ymd);
+       if(flexEmp == null) {
+          return null;
+       }
+        WtmFlexibleStdMgr flexStdMgr = flexStdMgrRepo.findById(flexEmp.getFlexibleStdMgrId()).get();
+        calcService.P_WTM_WORK_DAY_RESULT_CREATE_N(flexStdMgr, tenantId, enterCd, sabun, ymd, 0, sabun);
+		
+        List<WtmWorkDayResult> goOutList= workDayResultRepo.findByTenantIdAndEnterCdAndSabunAndTimeTypeCdAndYmdBetween(tenantId, enterCd, sabun, WtmApplService.TIME_TYPE_GOBACK, ymd, ymd);
+        
+        if(goOutList != null && goOutList.size() > 0) {
+            for(WtmWorkDayResult f : goOutList) {
+               System.out.println("goout send: " + f.toString());
+               //SimpleDateFormat dt = new SimpleDateFormat("yyyyMMddHHmmss");
+               empService.addWtmDayResultInBaseTimeType(
+                      tenantId
+                     , enterCd
+                    , ymd
+                    , sabun
+                    , f.getTimeTypeCd()
+                    , ""
+                    , f.getPlanSdate()
+                    , f.getPlanEdate()
+                    , null
+                    , "0"
+                    , false);
+            }
+         }
+        
+        System.out.println("::::::: calcApprDayInfo L:LLLLLL");
+        empService.calcApprDayInfo(tenantId, enterCd, ymd, ymd, sabun);
+        /*
+		*/
 		paramMap.put("tenantId", tenantId);
 		paramMap.put("enterCd", enterCd);
 		paramMap.put("sabun", sabun);
